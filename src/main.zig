b4f0478abf33471128da45e973544eedcc97d5e9
Yohai Ido <yohai@networx.com>
2025-04-16 12:15:06 +0300

    wip - start threads

--- src/main.zig
+++ src/main.zig
@@ -3,17 +3,29 @@ const net = std.net;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     try stdout.print("Logs from your program will appear here!\n", .{});
 
-    // Uncomment this block to pass the first stage
+    // threads
+    const thread_count = 4;
+    var threads: [thread_count]std.Thread = undefined;
+    for (0..4) |i| {
+        threads[i] = try std.Thread.spawn(.{}, handleConections, .{});
+        try stdout.print("Thread {} started\n", .{i});
+    }
+
+    for (threads) |thread| {
+        thread.join();
+    }
+}
+
+fn handleConections() !void {
+    const stdout = std.io.getStdOut().writer();
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
-    //
     while (true) {
         const connection = try listener.accept();
         try stdout.print("client connected!", .{});
